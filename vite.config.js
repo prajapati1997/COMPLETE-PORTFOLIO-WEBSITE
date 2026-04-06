f8c1d152ceeae871bf97908591ded4a6466e9405
prajapati1997 <123305891+prajapati1997@users.noreply.github.com>
2026-04-07 01:35:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-// vite.config.js
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: "/COMPLETE-PORTFOLIO-WEBSITE/",
-});
+  plugins: [react()],
+  base: './', // This ensures CSS/JS paths start with ./ instead of /
+})
