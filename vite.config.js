de7cfaac06d74f6f17d358c2712ce10fa3bb1108
prajapati1997 <123305891+prajapati1997@users.noreply.github.com>
2026-04-08 00:07:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 // vite.config.js
-import { defineConfig } from "vite";
+//import { defineConfig } from "vite";
 
-export default defineConfig({
-    base: "/COMPLETE-PORTFOLIO-WEBSITE/",
-});
+//export default defineConfig({
+   // base: "/COMPLETE-PORTFOLIO-WEBSITE/",
+//});
+export default {
+  base: './', // Use relative paths for assets
+  // ... other config
+}
